60f4186bd58b09e00fd981a328cad80f7306ce98
= <=>
2022-09-16 17:52:06 -0300

    adição do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import { Express } from "express";
+
+const app = express();
+
+const livros = [
+  {
+    id: 1,
+    Titulo: "Senhor dos Aneis",
+  },
+  {
+    id: 2,
+    Titulo: "O Hobbit",
+  },
+];
