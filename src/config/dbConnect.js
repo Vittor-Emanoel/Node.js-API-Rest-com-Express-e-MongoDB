32a31b0966e5cb0fff0ae7f3d8bf6b15a0e309ae
Vittor-Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2022-09-19 12:03:53 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://Vittor:300321@cluster0.ujzzcdb.mongodb.net/alura-node"
+  "mongodb+srv://Vittor:****@cluster0.ujzzcdb.mongodb.net/alura-node"
 );
 
 let db = mongoose.connection;
