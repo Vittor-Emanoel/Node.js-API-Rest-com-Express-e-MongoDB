521fdbcf223fcb70aef94ad3c8b65a25ffdf3b35
= <=>
2022-09-26 12:48:25 -0300

    voltando

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-//   "mongodb+srv://vittor:Vb@alura.tesy5xd.mongodb.net/alura-node"
-// );
+  "mongodb+srv://vittor:300321Vb@alura.tesy5xd.mongodb.net/alura-node"
+);
 
 let db = mongoose.connection;
 
