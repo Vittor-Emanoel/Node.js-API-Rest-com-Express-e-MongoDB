dcf7b4e4887f14ebcc0f693219ae8d2bbe23f23f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-15 00:48:14 -0300

    FEAT: const Port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Servidor rodando http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando http://localhost:${PORT}`);
 });
